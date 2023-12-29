381a4081dbb711d971f496c45be54aac5e992fd7
Prashant Pandey <103853523+Prashant684@users.noreply.github.com>
2023-12-29 18:12:18 +0000

    datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") 
+/*this will only run in browser inbuilt js engine. 
+And node.js has another way to achieve this*/
+
+// read documentation from 'mdn' (managed by mosilla) or 'tc39' (managed by ecma international)
+
+console.log(3
+    +3) //focus on code readability
+
+console.log("prashant")
+
+let name="prshant" //string 
+let age= 23 //number
+let isLoggenIn= false //boolean
+
+// number ==> 2 to power 53
+// bigint 
+// string ==> "" or ''
+// boolean ==> true / false
+// null ==> standalone value - representation of empty
+// undefined ==> not defined - value has not been defined
+// symbol ==> unique (learn about in react/figma)
+
+// object 
+
+console.log(typeof age);
+
+console.log(typeof null) //object
+console.log(typeof undefined) //unefined
