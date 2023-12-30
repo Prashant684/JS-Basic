540cff6b5d39c1281676db2da455be87a4825092
Prashant Pandey <103853523+Prashant684@users.noreply.github.com>
2023-12-30 09:38:20 +0000

    comparison

---
+++ 01_Basics/comparison.js
@@ -0,0 +1,18 @@
+/*
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+*/
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);  // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+console.log("2" == 2); // simple check check only the value
+console.log("2" === 2); //strict check checks the value as well as the data type
