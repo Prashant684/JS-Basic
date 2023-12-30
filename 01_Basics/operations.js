4b5b5c020cc0b2ed97ff4b6113b48caa40d466de
Prashant Pandey <103853523+Prashant684@users.noreply.github.com>
2023-12-30 07:58:14 +0000

    Operations

---
+++ 01_Basics/operations.js
@@ -0,0 +1,37 @@
+// ****************** Operations *****************
+
+let value = 3
+let negval= -value
+console.log(negval);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);*/
+
+let str1 = "Hello"
+let str2 = " Prashant"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+// ****  Prefix and Postfix  ****
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
