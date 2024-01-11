adb14a35e39a937808ecd1902f6ae56d60d7f485
Prashant Pandey <103853523+Prashant684@users.noreply.github.com>
2024-01-11 06:52:59 +0000

    Object 03 JSON

---
+++ 02_Basics/object03.js
@@ -0,0 +1,25 @@
+// destructuring objects
+const course = {
+    courseName: "Js in Hindi",
+    price: 999,
+    instructor: "Prashant"
+}
+// course.instructor
+
+const {instructor : inst} = course
+console.log(inst);
+
+//JSON 
+// example of JSOn file - https://api.github.com/users/hiteshchoudhary
+// https://randomuser.me/ - random api
+// https://jsonformatter.org/ - jason formatter
+
+
+
+{
+    name: "Prashnt";
+    course: "IOT";
+    price: "free";
+}
+
+//Sometimes the APIs are received in the form of arrays
